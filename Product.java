36d4ad61aef1807c94bf0537d6583d6ae2cd6c2c
Jonestg26 <152426524+Jonestg26@users.noreply.github.com>
2024-11-19 14:59:52 -0600

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,39 @@
+public class Product {
+    private String productId;
+    private String name;
+    private String category;
+    private double price;
+    
+    public Product(String productId, String name, String category, double price) {
+        this.productId = productId;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+    
+    public String getProductId() {
+        return productId;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getCategory() {
+        return category;
+    }
+    
+    public double getPrice() {
+        return price;
+    }
+    
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + productId + '\'' +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
